03228138bef259c4fb1310263044bb8eae9addd6
paidaxing <863199759@qq.com>
2025-05-19 22:52:28 +0800

    chore: alias config

--- threejs-editor/vite.config.js
+++ threejs-editor/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // 设置路径别名
+      components: fileURLToPath(new URL('./src/components', import.meta.url)),
+      composables: fileURLToPath(new URL('./src/composables', import.meta.url)),
+      assets: fileURLToPath(new URL('./src/assets', import.meta.url))
+    }
+  }
 })
